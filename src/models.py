6fef5bbb28f6b1a1beab003a0dc2b49a1d7191fa
AzimIzzudinMubarak <azimizzudinr0@gmail.com>
2026-04-11 13:52:51 +0200

    add models.py

---
+++ src/models.py
@@ -0,0 +1,18 @@
+from typing import Any, Literal
+from pydantic import BaseModel
+
+
+class FunctionParameter(BaseModel):
+    type: Literal["number", "string", "boolean"]
+
+
+class FunctionDefinition(BaseModel):
+    name: str
+    description: str
+    parameters: dict[str, FunctionParameter]
+
+
+class FunctionCall(BaseModel):
+    prompt: str
+    name: str
+    parameters: dict[str, Any]
